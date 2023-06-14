7d0cc17d0679a82ecafb8acd6b50883b8b4c42a1
Ashish Job <ashishjob104@gmail.com>
2023-06-14 15:36:46 -0500

    refactor: appropriate all experiences

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </div>
 
 
-      <div className="lg:absolute lg:top-1/3 lg:left-72 text-left text-green mt-12">
+      <div className="lg:absolute lg:top-1/3 lg:left-48 text-left text-green mt-12">
         <h1 className="text-center font-semibold sm:text-2xl md:text-2xl lg:text-2xl lg:text-left hover:underline">About Me</h1>
         
 
@@ -36,7 +36,7 @@ export default function Home() {
           check out all my work on GitHub!
         </h2>
 
-        <h2 className="sm:hidden font-base text-base pt-4 lg:text-left px-12">
+        <h2 className="sm:hidden font-base text-base pt-4 lg:text-left px-6">
           Hey there! My name is Ashish Job, and I am
           a rising sophomore majoring in Computer 
           Science with a minor in Mathematics at the University of Houston.<br /><br />
@@ -45,7 +45,7 @@ export default function Home() {
         </h2>
       </div>
 
-        <div className="lg:absolute flex justify-center items-center lg:bottom-1/3 lg:translate-y-2/3 lg:left-72 pb-4">
+        <div className="lg:absolute flex justify-center items-center lg:bottom-1/3 lg:translate-y-2/3 lg:left-48 pb-4">
           <div className="pt-3 pr-1">
             <a href="https://www.linkedin.com/in/ashish-job/" target="_blank">
               <Image
@@ -84,13 +84,24 @@ export default function Home() {
         </div>
 
 
-      <div className="lg:absolute hidden lg:block top-52 left-64 transform translate-y-44">
+      <div className="lg:absolute hidden lg:block top-52 left-40 transform translate-y-44">
         <hr className="w-1 h-80 bg-green border0 rounded dark:bg-green"></hr>
       </div>
 
-   
-      <div className="hidden md:absolute lg:block lg:absolute lg:left-1/2 lg:top-20 transform translate-y-1/4 translate-x-8">
-        <hr style={{ height: '600px' }} className="w-1 bg-green border0 rounded dark:bg-green"></hr>
+      <div className="hidden md:block lg:block md:px-8 lg:absolute lg:left-1/2 lg:top-44 transform translate-y-1/4 lg:translate-x-8">
+        <h1 className="sm:left-2 text-3xl font-semibold hover:underline text-green">
+          Experiences
+        </h1>
+      </div>
+
+      <div className="md:hidden sm:block">
+        <h1 className="pt-4 pl-4 text-3xl font-semibold hover:underline text-green">
+          Experiences
+        </h1>
+      </div>
+
+      <div className="hidden md:absolute lg:block lg:absolute lg:left-1/2 lg:top-4 transform translate-y-1/4 translate-x-8">
+        <hr style={{ height: '680px' }} className="w-1 bg-green border0 rounded dark:bg-green"></hr>
       </div>
 
       <div className="hidden lg:hidden md:absolute md:block md:top-56 md:left-40">
@@ -98,56 +109,51 @@ export default function Home() {
       </div>
 
 
-      <div className="lg:absolute lg:left-1/2 transform translate-x-8 sm:justify-center sm:items-center md:justify-center md:items-center top-52 py-2 right-12 text-green text-start">
-        <div className="py-3">
-          <h1 className="px-8 md:px-20 font-semibold text-xl md:text-2xl lg:text-2xl hover:underline pb-2">
-            Undergraduate Researcher @ SAIL Lab
-          </h1>
 
-          <h2 className="py-1 px-12 md:pl-28 pr-16 sm:text-lg md:text-xl">
-            I drew data to import into nerfstudio, a Neural Embedded Radiance Field software, to render the image into 3-Dimensions to detect flaws in civil infrastructure.
+      <div className="lg:absolute lg:left-1/2 top-52 pt-6 right-12 text-green">
+        <div className="">
+          <h1 className="px-8 md:px-20 font-semibold text-xl md:text-2xl lg:text-2xl hover:underline pb-2">
+            Undergraduate Researcher @ SAIL Lab 
+          </h1>
+          <h3 className='pl-8 md:pl-20 pb-2 font-semibold '>[May 2023 - Present]</h3>
+
+          <h2 className="px-12 md:pl-28 pr-16 sm:text-lg md:text-lg">
+            I utilized NERFstudio, a Neural Embedded Radiance Field software, to render images into 3-Dimensions to detect flaws in civil infrastructure.
           </h2>
         </div>
 
-        {/* <div className="py-3">
-          <h1 className="w-27 font-semibold text-2xl text-decoration-line: hover:underline pb-2">
+        <div className="">
+          <h1 className="py-4 px-8 md:px-20 font-semibold text-xl md:text-2xl lg:text-2xl hover:underline pb-2">
             Logistics Director @ DataCON
           </h1>
+          <h3 className='pl-8 md:pl-20 pb-2 font-semibold '>[March 2023 - Present]</h3>
 
-          <h2 className="py-1 pl-6 pr-16 text-lg">
-            I lead a team of 6 members to organize the hosting responsibilities in venue, food, and scheduling for the first Datathon at the University of Houston.
+          <h2 className="px-12 md:pl-28 pr-16 sm:text-lg md:text-lg">
+            I led the set up for the first Datathon at the University of Houston with the hosting responsibilities in venue booking, food catering, and scheduling .
           </h2>
         </div>
 
-        <div className="py-3">
-          <h1 className="w-27 font-semibold text-2xl text-decoration-line: hover:underline pb-2">
+        <div className="">
+          <h1 className="py-4 px-8 md:px-20 font-semibold text-xl md:text-2xl lg:text-2xl hover:underline pb-2">
             Cybersecurity Intern @ Chrysallis.AI
           </h1>
+          <h3 className='pl-8 md:pl-20 pb-2 font-semibold '>[March 2023 - May 2023]</h3>
 
-          <h2 className="py-1 pl-6 pr-16 text-lg">
-            I led a team held in charge of comprehensively comparing the company&apos;s security policies wth a gap analysis.
+          <h2 className="px-12 md:pl-28 pr-16 sm:text-lg md:text-lg">
+            I led a team held in charge of comprehensively comparing the company&apos;s security policies through gap analyses.
           </h2>
         </div>
 
-        <div className="py-3">
-          <h1 className="w-27 font-semibold text-2xl text-decoration-line: hover:underline pb-2">
-            Logistics Officer @ CodeRED
+        <div className="mb-12">
+          <h1 className="py-4 px-8 md:px-20 font-semibold text-xl md:text-2xl lg:text-2xl hover:underline pb-2">
+            Web Developer @ CougarCS Web Dev
           </h1>
+          <h3 className='pl-8 md:pl-20 pb-2 font-semibold '>[March 2023 - Present]</h3>
 
-          <h2 className="py-1 pl-6 pr-16 text-lg">
-            I worked as a logistics officer and handled securing food vendors, venue booking, and scheduling arrangements at Houston&apos;s largest hackathon.
-          </h2>
-        </div>
-
-        <div className="py-3">
-          <h1 className="w-27 font-semibold text-2xl text-decoration-line: hover:underline pb-2">
-            Web Developer @ CougarCS WebDev
-          </h1>
-
-          <h2 className="py-1 pl-6 pr-16 text-lg">
+          <h2 className="px-12 md:pl-28 pr-16 sm:text-lg md:text-lg">
             I worked to construct the frontend of the admin portal of the organization to allow for easier user interaction.
           </h2>
-        </div> */}
+        </div> 
       </div> 
     </main>
   );
