ae3d48e81a97423b196a6a08494da27fa8928666
Ashish Job <ashishjob104@gmail.com>
2023-06-16 01:18:43 -0500

    refactor: allow for mobile appropriate view

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,30 +8,30 @@ export default function Contact() {
 
       <NormalNavbar/>
 
-      <div className='flex flex-row justify-center mt-56 text-3xl lg:text-4xl font-semibold text-green mb-6'>
+      <div className='flex flex-row justify-center mt-36 md:mt-56 text-3xl lg:text-4xl font-semibold text-green mb-6'>
         CONTACT ME
     </div>
 
     <div className='flex flex-row justify-center mb-6'>  
-        <div className="relative w-1/3">
+        <div className="relative md:w-1/3 w-56">
             <input className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" placeholder="Name"/>
         </div>
     </div>
 
     <div className='flex flex-row justify-center mb-6'>  
-        <div className="relative w-1/3">
+        <div className="relative md:w-1/3 w-56">
             <input className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" placeholder="Email"/>
         </div>
     </div>
 
-    <div className='flex flex-row justify-center'>  
-        <div className="relative w-1/3 mb-6">
+    <div className='flex flex-row justify-center mb-6'>  
+        <div className="relative md:w-1/3 w-56">
             <textarea className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" placeholder="Message"/>
         </div>
     </div>
 
     <div className='flex flex-row justify-center'>  
-        <div className="relative w-1/6">
+        <div className="relative md:w-1/6 w-24">
             <button className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4 font-semibold hover:bg-textbox-green">
                 Submit
             </button>
