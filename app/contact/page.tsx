189b220217a3b4f63eebd5c300d54129c6a6e609
Ashish Job <ashishjob104@gmail.com>
2023-06-17 14:48:45 -0500

    feat: add loading spinner to button once clicked

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,51 +1,117 @@
-import NormalNavbar from "@/components/NavBar";
-import Image from "next/image";
-import Link from "next/link";
-import RootLayout from "../layout";
+"use client";
+import NormalNavbar from '@/components/NavBar';
+import { useState } from 'react';
 
-export default function Contact() {
+function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+
+      setName('');
+      setEmail('');
+      setMessage('');
+
+      setIsLoading(false);
+      setIsSent(true);
+
+      setTimeout(() => {
+        setIsSent(false);
+      }, 3000);
+    }, 2000);
+  };
+
   return (
-    <main className="min-w-screen bg-lightgreen flex min-h-screen flex-col">
+    <main className="flex flex-col min-h-screen min-w-screen bg-lightgreen">
       <NormalNavbar />
 
-      <div className="text-green mb-6 mt-36 flex flex-row justify-center text-3xl font-semibold md:mt-56 lg:text-4xl">
+      <div className='flex flex-row justify-center mt-36 md:mt-56 text-3xl lg:text-4xl font-semibold text-green mb-6'>
         CONTACT ME
       </div>
 
-      <div className="mb-6 flex flex-row justify-center">
-        <div className="relative w-56 md:w-1/3">
+      <div className='flex flex-row justify-center mb-6'>
+        <div className={`relative md:w-1/3 w-56 ${name ? 'border-green' : ''}`}>
           <input
-            className="border-1 border-green text-green block w-full rounded-lg bg-white p-4 text-sm"
+            className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4"
             placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
       </div>
 
-      <div className="mb-6 flex flex-row justify-center">
-        <div className="relative w-56 md:w-1/3">
+      <div className='flex flex-row justify-center mb-6'>
+        <div className={`relative md:w-1/3 w-56 ${email ? 'border-green' : ''}`}>
           <input
-            className="border-1 border-green text-green block w-full rounded-lg bg-white p-4 text-sm"
+            className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
       </div>
 
-      <div className="mb-6 flex flex-row justify-center">
-        <div className="relative w-56 md:w-1/3">
+      <div className='flex flex-row justify-center mb-6'>
+        <div className={`relative md:w-1/3 w-56 ${message ? 'border-green' : ''}`}>
           <textarea
-            className="border-1 border-green text-green block w-full resize-y rounded-lg bg-white p-4 text-sm"
+            className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4"
             placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </div>
       </div>
 
-      <div className="flex flex-row justify-center">
-        <div className="relative w-24 md:w-1/6">
-          <button className="border-1 border-green text-green hover:bg-textbox-green block w-full resize-y rounded-lg bg-white p-4 text-sm font-semibold">
-            Submit
+      <div className='flex flex-row justify-center'>
+        <div className="relative md:w-1/6 w-24">
+          <button
+            className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4 font-semibold hover:bg-textbox-green"
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <div className='flex flex-row justify-center'>
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 justify-center"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              </div>
+            ) : (
+              'Submit'
+            )}
           </button>
         </div>
       </div>
+
+      {isSent && (
+        <div className="flex flex-row font-semibold justify-center mt-4 text-green">
+          Message sent!
+        </div>
+      )}
     </main>
   );
 }
+
+export default Contact;
