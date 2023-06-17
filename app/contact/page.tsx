635b93b80dd867edd3bcd393b46edd8bf295776a
Ashish Job <ashishjob104@gmail.com>
2023-06-17 00:29:20 -0500

    feat: add experience positions

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,42 +1,47 @@
 import NormalNavbar from '@/components/NavBar';
 import Image from 'next/image';
 import Link from 'next/link';
+import RootLayout from '../layout';
 
 export default function Contact() {
   return (
     <main className="flex flex-col min-h-screen min-w-screen bg-lightgreen">
 
-      <NormalNavbar/>
-
-      <div className='flex flex-row justify-center mt-36 md:mt-56 text-3xl lg:text-4xl font-semibold text-green mb-6'>
-        CONTACT ME
-    </div>
-
-    <div className='flex flex-row justify-center mb-6'>  
-        <div className="relative md:w-1/3 w-56">
-            <input className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" placeholder="Name"/>
+        <NormalNavbar/>
+        
+        <div className='flex flex-row justify-center mt-36 md:mt-56 text-3xl lg:text-4xl font-semibold text-green mb-6'>
+            CONTACT ME
         </div>
-    </div>
 
-    <div className='flex flex-row justify-center mb-6'>  
-        <div className="relative md:w-1/3 w-56">
-            <input className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" placeholder="Email"/>
-        </div>
-    </div>
 
-    <div className='flex flex-row justify-center mb-6'>  
-        <div className="relative md:w-1/3 w-56">
-            <textarea className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" placeholder="Message"/>
+        <div className='flex flex-row justify-center mb-6'>  
+            <div className="relative md:w-1/3 w-56">
+                <input className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" 
+                placeholder="Name"/>
+            </div>
         </div>
-    </div>
 
-    <div className='flex flex-row justify-center'>  
-        <div className="relative md:w-1/6 w-24">
-            <button className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4 font-semibold hover:bg-textbox-green">
-                Submit
-            </button>
+        <div className='flex flex-row justify-center mb-6'>  
+            <div className="relative md:w-1/3 w-56">
+                <input className="bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" 
+                placeholder="Email"/>
+            </div>
         </div>
-    </div>
+
+        <div className='flex flex-row justify-center mb-6'>  
+            <div className="relative md:w-1/3 w-56">
+                <textarea className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4" 
+                placeholder="Message"/>
+            </div>
+        </div>
+
+        <div className='flex flex-row justify-center'>  
+            <div className="relative md:w-1/6 w-24">
+                <button className="resize-y bg-white border-1 border-green text-green text-sm rounded-lg block w-full p-4 font-semibold hover:bg-textbox-green">
+                    Submit
+                </button>
+            </div>
+        </div>
 
 
       
--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,6 +1,4 @@
 import NormalNavbar from '@/components/NavBar';
-import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Experience() {
   return (
@@ -9,15 +7,136 @@ export default function Experience() {
       <NormalNavbar/>
 
       <div className="Heading">
-        <div className="flex flex-row justify-center lg:justify-start font-semibold text-4xl text-green hover:underline
-         mt-24 lg:ml-24">
+        <div className="flex flex-row justify-center lg:justify-start font-bold text-4xl text-green underline
+         mt-8 lg:ml-24">
           <h1 className="">
             Experiences
           </h1>
         </div>
-        <div className='flex flex-row justify-center text-green text-4xl font-bold md:mt-12'>
-            Coming Soon...
-        </div>
+        
+        <div>
+        <div className='flex flex-col lg:flex-row justify-start items-center lg:items-start text-green mt-12 lg:ml-20'>
+            <a href="https://sail.cive.uh.edu/" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Undergraduate Research <br />Assistant @ SAIL Lab</h1>
+                <h6 className='mt-2 font-semibold'> [May 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                    Descriptions Coming Soon!!
+                    {/* I acquired data and utilized NERFstudio to detect anomalous comparisons between buildings pre and post disaster through computer vision and language modeling */}
+                </div>
+                </a>
+                
+                <a
+                href="https://www.qubitbyqubit.org/programs" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>UQN Member @ <br /> Qubit x Qubit</h1>
+                <h6 className='mt-2 font-semibold'> [May 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                    Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                <a
+                //href=""
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Logistics Director @ <br />DataCON</h1>
+                <h6 className='mt-2 font-semibold'> [April 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                
+
+                <a
+                href="https://uhcode.red/" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Logistics Officer @ <br />CodeRED</h1>
+                <h6 className='mt-2 font-semibold'> [April 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                <a
+                href="https://chrysallis.ai/" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Cybersecurity Intern @ <br />Chrysallis.AI</h1>
+                <h6 className='mt-2 font-semibold'> [March 2023 - May 2023] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+                
+            </div>
+
+            <div className='flex flex-col lg:flex-row justify-start items-center lg:items-start text-green mt-12 lg:ml-20'>
+                
+            <a href="https://cougarcs.com/" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Web Developer @ <br />CougarCS Web Dev</h1>
+                <h6 className='mt-2 font-semibold'> [March 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                <a
+                //href="" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Relations Officer @ <br />CougarAI</h1>
+                <h6 className='mt-2 font-semibold'> [April 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                <a
+                href="https://www.codecoogs.com/" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Teams Director @ <br />Code[Coogs]</h1>
+                <h6 className='mt-2 font-semibold'> [April 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                <a
+                href="https://cougarcs.com//" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>"Hacker" @ CougarCS<br /> InfoSec Team</h1>
+                <h6 className='mt-2 font-semibold'> [February 2023 - Present] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+                <a
+                href="https://www.codecoogs.com/" 
+            target="_blank" 
+            className='bg-white border-2 border-green text-green rounded-lg block w-72 h-72 p-4 mt-12 lg:mt-0 lg:ml-20 hover:bg-textbox-green'>
+                <h1 className='font-semibold text-xl'>Executive Intern  @ <br />Code[Coogs]</h1>
+                <h6 className='mt-2 font-semibold'> [January 2023 - May 2023] </h6>
+                <div className='flex flex-row text-base font-sm lg:ml-4 mt-2'>
+                Descriptions Coming Soon!!
+                </div>
+                </a>
+
+            </div>
+
+
+
+            </div>
+        
       </div>
 
 
